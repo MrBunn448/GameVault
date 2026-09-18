c0b4108e1238c8e58cb24ca99dfa48835e660102
Giovanni Schmidt <giovannischmidt1511zoef@gmail.com>
2026-09-18 16:21:30 +0200

    Initialize Spring Boot API project skeleton

---
+++ GameVaultAPI/src/test/java/com/gamevault/api/GameVaultApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamevault.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameVaultApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
